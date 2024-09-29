b3da188ff3c74c946a45f1c70fe68c3a21ae5318
C Zhang <rrg517@login2.viking2.yor.alces.network>
2024-09-29 17:14:09 +0100

    batchsize=12 for NVIDIA A40, add ddp config

--- tutorial_train_sd21.py
+++ tutorial_train_sd21.py
@@ -9,7 +9,7 @@ from cldm.model import create_model, load_state_dict
 
 # Configs
 resume_path = './models/control_sd21_ini.ckpt'
-batch_size = 1
+batch_size = 12
 logger_freq = 300
 learning_rate = 1e-5
 sd_locked = True
@@ -28,7 +28,8 @@ model.only_mid_control = only_mid_control
 dataset = MyDataset()
 dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
 logger = ImageLogger(batch_frequency=logger_freq)
-trainer = pl.Trainer(gpus=1, precision=32, callbacks=[logger])
+# trainer = pl.Trainer(gpus=1, precision=32, callbacks=[logger])
+trainer = pl.Trainer(strategy="ddp", accelerator="gpu", gpus=2, precision=32, callbacks=[logger])
 
 
 # Train!
