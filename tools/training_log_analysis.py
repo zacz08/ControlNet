5988c5fc22fd17c6ac23753eb3903a1643790db0
zacz08 <zacharyzhang95@163.com>
2024-10-02 15:08:34 +0100

    tool to generate training curve

--- tools/training_log_analysis.py
+++ tools/training_log_analysis.py
@@ -2,7 +2,7 @@ import re
 import matplotlib.pyplot as plt
 
 # 读取日志文件
-log_file = 'your_log_file.log'  # 将这个替换为你的实际日志文件名
+log_file = '/home/zc/ControlNet/tools/training_log_unlock.log'  # 将这个替换为你的实际日志文件名
 
 # 打开并读取日志文件内容
 with open(log_file, 'r') as f:
@@ -18,14 +18,17 @@ losses = [float(loss) for loss in losses]
 # 计算总共多少个iterations
 total_iterations = len(losses)
 
+# 设置整体字体大小
+plt.rcParams.update({'font.size': 20})  # 这里设置为16，你可以根据需要调整大小
+
 # 绘制曲线图
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(20, 12))
 plt.plot(losses, label='Training Loss')
 
-# 添加每142个iteration处的竖线
-epoch_interval = 142  # 每142个iteration为一个epoch
-for i in range(epoch_interval, total_iterations, epoch_interval):
-    plt.axvline(x=i, color='r', linestyle='--', alpha=0.6)
+# # 添加每142个iteration处的竖线
+# epoch_interval = 142  # 每142个iteration为一个epoch
+# for i in range(epoch_interval, total_iterations, epoch_interval):
+#     plt.axvline(x=i, color='r', linestyle='--', alpha=0.6)
 
 # 设置图表的标签和标题
 plt.xlabel('Iterations')
