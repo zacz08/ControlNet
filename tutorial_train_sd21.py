674a5d35d8b58831a88e50cd02eed937e26a7538
zacz08 <zacharyzhang95@163.com>
2024-10-10 13:06:46 +0100

    add comments to indicate learning rate and batch size

--- tutorial_train_sd21.py
+++ tutorial_train_sd21.py
@@ -8,11 +8,18 @@ from cldm.model import create_model, load_state_dict
 
 
 # Configs
-resume_path = './models/control_sd21_ini.ckpt'
-batch_size = 12
+resume_path = './lightning_logs/version_11797349/checkpoints/epoch=12-step=41495.ckpt'
+
+# sd_locked = True  --> 12
+# sd_locked = False --> 10
+batch_size = 10
+
 logger_freq = 300
-learning_rate = 1e-5
-sd_locked = True
+
+# sd_locked = True  --> 1e-5
+# sd_locked = False --> 2e-6
+learning_rate = 2e-6
+sd_locked = False
 only_mid_control = False
 
 
@@ -26,10 +33,10 @@ model.only_mid_control = only_mid_control
 
 # Misc
 dataset = MyDataset()
-dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=False)
+dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
 logger = ImageLogger(batch_frequency=logger_freq)
 # trainer = pl.Trainer(gpus=1, precision=32, callbacks=[logger])
-trainer = pl.Trainer(strategy="ddp", accelerator="gpu", gpus=2, precision=32, callbacks=[logger])
+trainer = pl.Trainer(strategy="ddp", accelerator="gpu", gpus=3, precision=32, callbacks=[logger])
 
 
 # Train!
